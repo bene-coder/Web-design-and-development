a6b0eca9ad31c5e65f121ccba40f90004a10febb
bene-coder <onyiriaghabenedict@gmail.com>
2022-10-19 15:34:06 +0100

    Added Article preview component to the repository

---
+++ article-preview-component-master/app.js
@@ -0,0 +1,7 @@
+var dropBtn = document.querySelector('.dropbtn');
+const dropDown = document.querySelector('.dropcontent');
+
+
+    dropBtn.addEventListener('click', ()=> {
+        dropDown.classList.toggle("open");
+    });
